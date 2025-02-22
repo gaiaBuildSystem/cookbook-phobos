ce73a3a33702243e8ec8cfa2bef082285e39cf5f
Matheus Castello <matheus@castello.eng.br>
2025-02-22 09:38:22 -0300

    recipes-debian: Add sshd support
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ cookbook/recipes-debian/bookworm/systemd.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import os
+import shutil
+import subprocess
+
+# get the environment
+
+ARCH = os.getenv('ARCH')
+MACHINE = os.getenv('MACHINE')
+BUILD_PATH = os.getenv('BUILD_PATH')
+USER_PASSWD = os.getenv('USER_PASSWD')
+USER = os.getenv('USER')
+PSWD = os.getenv('PSWD')
+USER_LOGIN_USER = os.getenv('USER_LOGIN_USER')
+INITRAMFS_PATH = os.getenv('INITRAMFS_PATH')
+
+IMAGE_MNT_BOOT = f"{BUILD_PATH}/tmp/{MACHINE}/mnt/boot"
+IMAGE_MNT_ROOT = f"{BUILD_PATH}/tmp/{MACHINE}/mnt/root"
+os.environ['IMAGE_MNT_BOOT'] = IMAGE_MNT_BOOT
+os.environ['IMAGE_MNT_ROOT'] = IMAGE_MNT_ROOT
+
+# get the actual script path
+_path = os.path.dirname(os.path.realpath(__file__))
+
+print(f"configuring systemd ...")
+
+# chroot cmds
+systemd_cmd = (
+    f"echo {USER_PASSWD} | sudo -k -S "
+    f"chroot {IMAGE_MNT_ROOT} /bin/bash -c \""
+    f"systemctl enable sshd --force"
+    f"\""
+)
+
+subprocess.run(
+    systemd_cmd,
+    shell=True,
+    check=True,
+    executable="/bin/bash",
+    env=os.environ
+)
+
+print(f"systemd config ok!")
