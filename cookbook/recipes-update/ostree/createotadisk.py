f9ab24f6b7b145e51c88520bd9c6997ad41f5901
Matheus Castello <matheus@castello.eng.br>
2025-05-28 21:08:26 -0300

    recipes-update: ostree: Fix leftover code from createotadisk.py
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-update/ostree/createotadisk.py
+++ cookbook/recipes-update/ostree/createotadisk.py
@@ -25,45 +25,12 @@ os.environ['IMAGE_MNT_ROOT'] = IMAGE_MNT_ROOT
 # get the actual script path
 _path = os.path.dirname(os.path.realpath(__file__))
 
-print(f"Create the .img for the ostree based distro")
+print("Create the .img for the ostree based distro")
 
 # run the createotadisk.sh script
 _cmds = [
     f"{_path}/createotadisk.sh"
 ]
-
-# _cmds = []
-
-# # check if the .img file exists
-# _img_ota_path = f"{BUILD_PATH}/tmp/{MACHINE}/deploy/{MACHINE}-ota-{DISTRO_MAJOR}-{DISTRO_MINOR}-{DISTRO_PATCH}.img"
-
-# if os.path.exists(_img_ota_path):
-#     _cmds += [
-#         f"rm -rf {_img_ota_path}"
-#     ]
-
-# _os_tree_deploy_path = f"{BUILD_PATH}/tmp/{MACHINE}/ostree/deploy"
-
-# _cmds += [
-#     # create the .img based on the distro .img
-#     f"cp {BUILD_PATH}/tmp/{MACHINE}/deploy/{MACHINE}-{DISTRO_MAJOR}-{DISTRO_MINOR}-{DISTRO_PATCH}.img {_img_ota_path}",
-
-#     # mount it
-#     f"mkdir -p {IMAGE_MNT_BOOT}-ota",
-#     f"mkdir -p {IMAGE_MNT_ROOT}-ota",
-#     f"mount -o loop {_img_ota_path} {IMAGE_MNT_BOOT}-ota",
-#     f"mount -o loop {_img_ota_path} {IMAGE_MNT_ROOT}-ota",
-
-#     # remove all from rootfs
-#     f"rm -rf {IMAGE_MNT_ROOT}-ota/*",
-
-#     # clone all from ostree to rootfs
-#     f"rsync -a {_os_tree_deploy_path}/ {IMAGE_MNT_ROOT}-ota/",
-
-#     # unmount it
-#     f"umount {IMAGE_MNT_BOOT}-ota",
-#     f"umount {IMAGE_MNT_ROOT}-ota"
-# ]
 
 for _cmd in _cmds:
     print(f"\033[94mRunning: {_cmd}\033[0m")
@@ -74,8 +41,7 @@ for _cmd in _cmds:
         shell=True,
         check=True,
         executable="/bin/bash",
-        input=f"{USER_PASSWD}\n".encode(),
         env=os.environ
     )
 
-print(f"OTA .img created")
+print("OTA .img created")
