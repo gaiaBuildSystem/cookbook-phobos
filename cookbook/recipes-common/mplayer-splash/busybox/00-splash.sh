95fa4f8b9fb8b738c5719a35478c830e38b19f57
Matheus Castello <matheus@castello.eng.br>
2025-10-28 13:45:20 -0300

    recipes-common: mplayer-splash: switch to drm video output
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-common/mplayer-splash/busybox/00-splash.sh
+++ cookbook/recipes-common/mplayer-splash/busybox/00-splash.sh
@@ -3,11 +3,11 @@
 echo "[initramfs] initializing video splash screen..."
 
 echo "[initramfs] modeset initializing..."
-/usr/mplayer-splash/mplayer -nosound -vo fbdev2 /usr/mplayer-splash/1.mp4 >/dev/null 2>&1
+/usr/mplayer-splash/mplayer -nosound -vo drm /usr/mplayer-splash/1.mp4 >/dev/null 2>&1
 
 # I hate to do this, but some screens need some time to proper show the first frame
 sleep 1
 
 echo "[initramfs] splash initialized..."
-/usr/mplayer-splash/mplayer -loop 0 -nosound -vo fbdev2 /usr/mplayer-splash/2.mp4 >/dev/null 2>&1 &
+/usr/mplayer-splash/mplayer -loop 0 -nosound -vo drm /usr/mplayer-splash/2.mp4 >/dev/null 2>&1 &
 echo "[initramfs] splash loop..."
