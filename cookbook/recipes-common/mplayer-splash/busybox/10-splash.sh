6939cc15981e37050e790a5690977e06a90c312a
Matheus Castello <matheus@castello.eng.br>
2025-11-14 00:21:12 -0300

    recipes-common: mplayer-splash: improve splash stop timing and fix vo
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-common/mplayer-splash/busybox/10-splash.sh
+++ cookbook/recipes-common/mplayer-splash/busybox/10-splash.sh
@@ -2,5 +2,5 @@
 
 echo "[initramfs] initializing video splash screen..."
 
-/usr/mplayer-splash/mplayer -nosound -vo -fixed-vo -loop 0 -loop-start 1.57 /usr/mplayer-splash/1.mp4 >/dev/null 2>&1 &
+/usr/mplayer-splash/mplayer -nosound -vo drm -fixed-vo -loop 0 -loop-start 1.57 /usr/mplayer-splash/1.mp4 >/dev/null 2>&1 &
 echo "[initramfs] splash loop..."
