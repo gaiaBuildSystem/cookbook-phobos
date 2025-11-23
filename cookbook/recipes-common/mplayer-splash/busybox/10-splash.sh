ba1bbfb10017a1f950bd49b1da40c360efa941d6
Matheus Castello <matheus@castello.eng.br>
2025-11-23 20:59:16 -0300

    recipes-common: mplayer-splash: Add drmset call to force set mode
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-common/mplayer-splash/busybox/10-splash.sh
+++ cookbook/recipes-common/mplayer-splash/busybox/10-splash.sh
@@ -7,5 +7,9 @@ mkdir -p /dev/splash
 mount -t tmpfs tmpfs /dev/splash
 cp /usr/mplayer-splash/1.mp4 /dev/splash/1.mp4
 
+# force set mode
+/bin/drmset 0
+
+# good to go
 /usr/mplayer-splash/mplayer -nosound -vo drm -fixed-vo -loop 0 -loop-start 1.57 /dev/splash/1.mp4 >/dev/null 2>&1 &
 echo "[initramfs] splash loop..."
