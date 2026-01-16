546b683b955d0c84403aaa2ca440fe69aea5d99f
Matheus Castello <matheus@castello.eng.br>
2026-01-16 11:17:15 -0300

    mplayer-splash: busybox: Add support to change the default drm device
    For Raspberry Pi 5 it uses the /dev/dri/card1 device instead of card0.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-common/mplayer-splash/busybox/10-splash.sh
+++ cookbook/recipes-common/mplayer-splash/busybox/10-splash.sh
@@ -2,14 +2,22 @@
 
 echo "[initramfs] initializing video splash screen..."
 
+# choose drm card based on machine map; env overrides
+machine_id="${MACHINE:-unknown}"
+case "${machine_id}" in
+	rpi5b) card_id=1 ;;
+	*) card_id=0 ;;
+esac
+
+
 # copy it to the /dev/tmpfs
 mkdir -p /dev/splash
 mount -t tmpfs tmpfs /dev/splash
 cp /usr/mplayer-splash/1.mp4 /dev/splash/1.mp4
 
 # force set mode
-/bin/drmset 0
+/bin/drmset ${card_id}
 
 # good to go
-/usr/mplayer-splash/mplayer -nosound -vo drm -fixed-vo -loop 0 -loop-start 1.57 /dev/splash/1.mp4 >/dev/null 2>&1 &
+/usr/mplayer-splash/mplayer -nosound -vo drm:/dev/dri/card${card_id} -fixed-vo -loop 0 -loop-start 1.57 /dev/splash/1.mp4 >/dev/null 2>&1 &
 echo "[initramfs] splash loop..."
