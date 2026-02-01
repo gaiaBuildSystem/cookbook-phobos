d1284e0c7b484b58a3d371f4857f13b934dcbfba
Matheus Castello <matheus@castello.eng.br>
2026-01-31 21:47:03 -0300

    cookbook: all: Add SBOM generation recipes for several components
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ cookbook/recipes-common/mplayer-splash/mplayer-sbom.ts
@@ -0,0 +1,114 @@
+#!/opt/bun/bin/bun
+
+import PATH from "path"
+import FS from "fs"
+import logger from "node-color-log"
+import { PackageURL } from 'packageurl-js'
+import * as CDX from "@cyclonedx/cyclonedx-library"
+import { execSync, spawnSync } from "child_process"
+
+
+const ARCH = process.env.ARCH as string
+const MACHINE = process.env.MACHINE as string
+const MAX_IMG_SIZE = process.env.MAX_IMG_SIZE as string
+const BUILD_PATH = process.env.BUILD_PATH as string
+const DISTRO_MAJOR = process.env.DISTRO_MAJOR as string
+const DISTRO_MINOR = process.env.DISTRO_MINOR as string
+const DISTRO_PATCH = process.env.DISTRO_PATCH as string
+const USER_PASSWD = process.env.USER_PASSWD as string
+const DO_SBOM = process.env.DO_SBOM as string
+
+// get the actual script path, not the process.cwd
+const _path = PATH.dirname(process.argv[1])
+
+const IMAGE_PATH =
+    `${BUILD_PATH}/tmp/${MACHINE}/deploy/${MACHINE}-${DISTRO_MAJOR}-${DISTRO_MINOR}-${DISTRO_PATCH}.img`
+process.env.IMAGE_PATH = IMAGE_PATH
+
+const IMAGE_MNT_BOOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/boot`
+const IMAGE_MNT_ROOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/root`
+const SBOM_OUTPUT_PATH = `${BUILD_PATH}/tmp/${MACHINE}/sbom`
+process.env.IMAGE_MNT_BOOT = IMAGE_MNT_BOOT
+process.env.IMAGE_MNT_ROOT = IMAGE_MNT_ROOT
+
+if (DO_SBOM === "true") {
+    logger.info("Generating SBOM for mplayer ...")
+
+    // get the version
+    const app_src_path = PATH.join(
+        BUILD_PATH,
+        "tmp",
+        `${MACHINE}`,
+        "mplayer"
+    )
+
+    let mplayer_version = "unknown"
+    mplayer_version = execSync(
+        "cat VERSION",
+        {
+            cwd: app_src_path,
+            encoding: "utf-8",
+            env: process.env
+        }
+    ).trim()
+
+    const _cdx_sbom = new CDX.Models.Bom()
+    _cdx_sbom.metadata.component = new CDX.Models.Component(
+        CDX.Enums.ComponentType.OperatingSystem,
+        "Linux Kernel",
+        {
+            version: `${mplayer_version}`,
+        }
+    )
+    const _tools_component = new CDX.Models.Component(
+        CDX.Enums.ComponentType.Application,
+        "gaia"
+    )
+    _tools_component.version = "0.0.0"
+    _tools_component.externalReferences.add(
+        new CDX.Models.ExternalReference(
+            "https://github.com/gaiaBuildSystem/gaia",
+            CDX.Enums.ExternalReferenceType.Website,
+        )
+    )
+    _cdx_sbom.metadata.tools.components.add(
+        _tools_component
+    )
+    _cdx_sbom.components.add(
+        new CDX.Models.Component(
+            CDX.Enums.ComponentType.Library,
+            "mplayer",
+            {
+                version: `${mplayer_version}`,
+                cpe: `cpe:2.3:*:*:mplayer:${mplayer_version}:*:*:*:*:*:*:*`,
+                purl: PackageURL.fromString(`pkg:generic/mplayer@${mplayer_version}`)
+            }
+        )
+    )
+
+    // write the SBOM to file
+    if (!FS.existsSync(SBOM_OUTPUT_PATH)) {
+        FS.mkdirSync(SBOM_OUTPUT_PATH, { recursive: true })
+    }
+
+    const sbom_file_path = PATH.join(
+        SBOM_OUTPUT_PATH,
+        `mplayer-sbom.json`
+    )
+
+    const serialized = new CDX.Serialize.JsonSerializer(
+        new CDX.Serialize.JSON.Normalize.Factory(
+            CDX.Spec.Spec1dot6
+        )
+    ).serialize(_cdx_sbom)
+
+    FS.writeFileSync(
+        sbom_file_path,
+        JSON.stringify(JSON.parse(serialized), null, 2),
+        "utf-8"
+    )
+
+    logger.success("SBOM generation for mplayer completed successfully.")
+}
+
+process.exit(0)
---
+++ cookbook/recipes-toradex/aktualizr-torizon/README.md
@@ -0,0 +1,9 @@
+# aktualizr-torizon Recipe
+
+This directory contains a Gaia cookbook for building the Torizon team customization of Aktualizr.
+
+As PhobOS is designed to be compatible with the Torizon update system, this recipe just build and installs the Torizon version of Aktualizr.
+
+## SBOM
+
+This cookbook does not need a specific recipe for SBOM generation because the build process generates a .deb package that at the end of the Gaia build will be included in the final SBOM.
